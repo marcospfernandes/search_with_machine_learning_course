e213dcf34f5320ea104a9c8c8578ad285e23eca9
marcospfernandes <marcospqf@gmail.com>
2022-07-04 06:46:35 +0000

    Level 1 + Level 2

--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -1,12 +1,23 @@
 import argparse
 import os
 import random
+import re
+
+from nltk.stem.snowball import EnglishStemmer
 import xml.etree.ElementTree as ET
+
 from pathlib import Path
 
+stemmer = EnglishStemmer()
+
 def transform_name(product_name):
-    # IMPLEMENT
-    return product_name
+    # Converting product name to lower
+    product_name = product_name.lower()
+    # Removing non alpha characteres
+    product_name=re.sub(r'[\W_]+', '', product_name)
+    # Removing whitespaces 
+    product_name = re.sub(r"(?a:^\s+|\s+$)",'', product_name) 
+    return " ".join(map(stemmer.stem, product_name.split(" ")))
 
 # Directory for product data
 directory = r'/workspace/datasets/product_data/products/'
@@ -23,7 +34,7 @@ general.add_argument("--label", default="id", help="id is default and needed for
 general.add_argument("--sample_rate", default=1.0, type=float, help="The rate at which to sample input (default is 1.0)")
 
 # IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
-general.add_argument("--min_products", default=0, type=int, help="The minimum number of products per category (default is 0).")
+general.add_argument("--min_products", default=500, type=int, help="The minimum number of products per category (default is 500).")
 
 args = parser.parse_args()
 output_file = args.output
@@ -42,7 +53,7 @@ if args.label == 'name':
     names_as_labels = True
 
 print("Writing results to %s" % output_file)
-with open(output_file, 'w') as output:
+with open('/tmp/labeled_products.txt', 'w') as output:
     for filename in os.listdir(directory):
         if filename.endswith(".xml"):
             print("Processing %s" % filename)
@@ -66,3 +77,7 @@ with open(output_file, 'w') as output:
                       # Replace newline chars with spaces so fastText doesn't complain
                       name = child.find('name').text.replace('\n', ' ')
                       output.write("__label__%s %s\n" % (cat, transform_name(name)))
+
+df = pandas.read_csv('/tmp/labeled_products.txt')
+df = df.str.str.split(' ', n=1, expand=True).groupby(axis=0).filter(lambda x: len(x) >= min_filter_cut)
+df.to_csv(output_file, sep='\t', header=None, index=False) 
---
+++ week2/run-training.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# LEVEL 1
+echo 'Running level 1...'
+echo 'Creating Training Data and Filtering Categories...'
+python week2/createContentTrainingData.py --output /workspace/datasets/fasttext/labeled_products.txt --min_products 500
+echo 'Shuffling the Data...'
+shuf /workspace/datasets/fasttext/labeled_products.txt > /workspace/datasets/fasttext/shuffled_labeled_products.txt
+echo 'Generating Test Data...'
+trail -n10000 /workspace/datasets/fasttext/shuffled_labeled_products.txt > /workspace/datasets/fasttext/training_data.txt
+echo 'Generating train Data...'
+head -n10000 /workspace/datasets/fasttext/shuffled_labeled_products.txt > /workspace/datasets/fasttext/test_data.txt
+~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/fasttext/training_data.txt -output /workspace/datasets/fasttext/product_classifier -wordNgrams 2 -lr 1 -epoch 25
+~/fastText-0.9.2/fasttext test /workspace/datasets/fasttext/product_classifier.bin /workspace/datasets/fasttext/test_data.txt 1
+
+
+# LEVEL 2
+echo 'Running level 2...'
+cut -d$'\t' -f2- /workspace/datasets/fasttext/shuffled_labeled_products.txt > $FT_DIR/normalized_titles.txt
+~/fastText-0.9.2/fasttext skipgram -input $FT_DIR/normalized_titles.txt -output $FT_DIR/title_model -minCount 20 -epoch 25
+~/fastText-0.9.2/fasttext nn $FT_DIR/title_model.bin
